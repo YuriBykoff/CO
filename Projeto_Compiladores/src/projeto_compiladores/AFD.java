cf1eb0f0e7527c2d79df48492c76246e8d47093a
Yuri Bykoff <160950966+YuriBykoff@users.noreply.github.com>
2024-10-14 19:38:17 -0300

    Add files via upload

---
+++ Projeto_Compiladores/src/projeto_compiladores/AFD.java
@@ -0,0 +1,7 @@
+package projeto_compiladores;
+
+import java.text.CharacterIterator;
+
+public abstract class AFD {
+    public abstract Token evaluate(CharacterIterator code);
+}
---
+++ Projeto_Compiladores/src/projeto_compiladores/id.java
@@ -0,0 +1,29 @@
+package projeto_compiladores;
+
+import java.text.CharacterIterator;
+
+public class id extends AFD{
+    
+    @Override
+    public Token evaluate(CharacterIterator code) {
+        StringBuilder lexema = new StringBuilder();
+        char currentChar = code.current();
+
+        //Verifica se o primeiro caractere é uma letra ou underline
+        if (Character.isLetter(currentChar) || currentChar == '_') {
+            lexema.append(currentChar);
+            code.next();
+
+            //Continua lendo enquanto for letra, dígito ou underscore
+            while (Character.isLetterOrDigit(code.current()) || code.current() == '_') {
+                lexema.append(code.current());
+                code.next();
+            }
+
+            return new Token("id", lexema.toString());
+        }
+
+        //Se não for um identificador válido, retorna null
+        return null;
+    }
+}
