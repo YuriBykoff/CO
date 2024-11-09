f203358d285a38864a30a8d13e8461129e1023aa
Yuri Bykoff <160950966+YuriBykoff@users.noreply.github.com>
2024-11-09 14:25:12 -0300

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO, emit
 import subprocess
 import os
 import threading
-import time
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -30,43 +29,36 @@ def execute_code(data):
     code = data.get('code')
     with open(input_file_path, 'w') as file:
         file.write(code)
-    print(f"Arquivo .txt atualizado em: {input_file_path}")
+    emit('output', {'message': f"Arquivo .txt atualizado em: {input_file_path}", 'type': 'debug'})
 
     # Executa o compilador principal para gerar Main.java
     execute_translator_command = f'java -cp "{build_path}" projeto_compiladores.Projeto_Compiladores'
-    print(f"Executando comando de tradução: {execute_translator_command}")
-    
     translator_process = subprocess.run(
         execute_translator_command, shell=True, cwd=project_path,
         capture_output=True, text=True
     )
 
     if translator_process.returncode != 0:
-        print(f"Erro ao executar Projeto_Compiladores: {translator_process.stderr}")
-        emit('output', f"Erro ao executar Projeto_Compiladores:\n{translator_process.stderr}")
+        emit('output', {'message': f"Erro ao executar Projeto_Compiladores:\n{translator_process.stderr}", 'type': 'error'})
         return
 
     # Verifica se Main.java foi gerado no diretório raiz
     if not os.path.exists(main_java_path):
-        print(f"Erro: Main.java não foi gerado em {project_path}")
-        emit('output', f"Erro: Main.java não foi gerado corretamente.")
+        emit('output', {'message': f"Erro: Main.java não foi gerado corretamente.", 'type': 'error'})
         return
 
     # Verifica se Main.java tem conteúdo
     if os.path.getsize(main_java_path) == 0:
-        print("Erro: Código inválido: o Main.java não foi gerado corretamente.")
-        emit('output', "Código inválido: o Main.java não foi gerado corretamente.")
+        emit('output', {'message': "Erro: Código inválido: o Main.java não foi gerado corretamente.", 'type': 'error'})
         return
 
     # Exclui o Main.class antigo para garantir que a versão anterior não seja executada
     if os.path.exists(main_class_path):
         os.remove(main_class_path)
-        print("Main.class antigo excluído.")
+        emit('output', {'message': "Main.class antigo excluído.", 'type': 'debug'})
 
     # Compila o novo Main.java gerado no diretório raiz
     compile_main_command = f'javac -d "{build_path}" "{main_java_path}"'
-    print(f"Compilando Main.java com o comando: {compile_main_command}")
-    
     compile_main_process = subprocess.run(
         compile_main_command,
         shell=True,
@@ -76,17 +68,13 @@ def execute_code(data):
     )
 
     if compile_main_process.returncode != 0:
-        emit('output', "Erro ao compilar Main.java:\n" + compile_main_process.stderr)
-        print(f"Erro ao compilar Main.java: {compile_main_process.stderr}")
+        emit('output', {'message': "Erro ao compilar Main.java:\n" + compile_main_process.stderr, 'type': 'error'})
         return
     else:
-        emit('output', 'Compilação de Main.java bem-sucedida.\n')
-        print("Compilação de Main.java bem-sucedida.")
+        emit('output', {'message': 'Compilação de Main.java bem-sucedida.\n', 'type': 'debug'})
 
     # Executa o Main.class
     execute_main_command = f'java -cp "{build_path}" Main'
-    print(f"Executando Main com o comando: {execute_main_command}")
-    
     process = subprocess.Popen(
         execute_main_command, 
         shell=True, 
@@ -100,7 +88,7 @@ def execute_code(data):
     # Thread para enviar o output do Main.java em tempo real para o cliente
     def send_output():
         for line in process.stdout:
-            socketio.emit('output', line)
+            socketio.emit('output', {'message': line, 'type': 'default-output'})
 
     threading.Thread(target=send_output).start()
 
